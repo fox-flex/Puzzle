b6d459996633020bb9b7a5a79021a53e3ab0c2ec
Fox <you@example.com>
2021-02-09 16:08:54 +0200

    create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Puzzle game
+This project is lab #1 task #2 is 2 semester
+of Basic programing in UCU CS 2021
---
+++ skyscrapers.py
@@ -0,0 +1,8 @@
+"""
+module for working with puzzle game
+git repo: https://github.com/fox-flex/puzzle
+"""
+
+
+if __name__ == "__main__":
+    print('Hello, World!')
