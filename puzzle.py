73dd110a4ead29ce67c6a535120f7119a2d91849
Fox <you@example.com>
2021-02-09 16:17:17 +0200

    create structure of puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,26 @@
+"""
+module for working with puzzle game
+git repo: https://github.com/fox-flex/puzzle
+"""
+
+
+def check_horizontal(board: list) -> bool:
+    pass
+
+
+def check_vertical(board: list) -> bool:
+    pass
+
+
+def check_color(board: list) -> bool:
+    pass
+
+
+def validate_board(board: list) -> bool:
+    is_correct = True
+
+    return is_correct
+
+
+if __name__ == "__main__":
+    print('Hello, World!')
--- skyscrapers.py
+++
@@ -1,8 +0,0 @@
-"""
-module for working with puzzle game
-git repo: https://github.com/fox-flex/puzzle
-"""
-
-
-if __name__ == "__main__":
-    print('Hello, World!')
